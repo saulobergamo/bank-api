9bab1e7082c477694e8c6d40c3836bbc0db3ff67
Saulo Bergamo <saulobergamo@hotmail.com>
2021-10-18 11:42:49 -0300

    Revert "bank-api"
    This reverts commit 77630a7497f92b63fa3c5f7da6dcf1a3d740b3e5.

--- src/main/kotlin/com/eprogramar/bankapi/repository/AccountRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.eprogramar.bankapi.repository
-
-import com.eprogramar.bankapi.model.Account
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface AccountRepository : JpaRepository<Account, Long> {
-}
